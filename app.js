2583c605f37516c4ff4480e997d064d27c504b4e
inventory-kk <syamilham7@gmail.com>
2026-08-29 12:05:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,11 @@
         if (!this.data.items.length) this.loadDemoData();
         
         // Auto set user sebagai Manager
-        this.currentUser = { username: 'Manager', role: 'MANAGER' };
+        this.currentUser = { username: 'Manager', role: 'MANAGER' };    closeAllModals() {
+        document.querySelectorAll('.modal').forEach(m => m.style.display = 'none');
+        document.getElementById('modal-overlay').style.display = 'none';
+    },
+
         localStorage.setItem('kk_user', JSON.stringify(this.currentUser));
         this.switchView('dashboard');
     },
